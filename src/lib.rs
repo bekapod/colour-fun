0d06c8081904aae0a59e58c6e15aec4d58c4a32b
Becky <bckyjns@gmail.com>
2020-07-22 17:06:21 +0100

    ✨add function for converting hex codes to rgb representation

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,70 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[derive(Debug, PartialEq, Eq)]
+pub struct RgbColour {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl RgbColour {
+    pub fn new(red: u8, green: u8, blue: u8) -> RgbColour {
+        RgbColour { red, green, blue }
+    }
+}
+
+pub enum ErrorCode {
+    InvalidHexCharacter,
+    InvalidHexLength,
+}
+
+impl std::convert::From<ErrorCode> for JsValue {
+    fn from(error: ErrorCode) -> JsValue {
+        match error {
+            ErrorCode::InvalidHexCharacter => {
+                JsValue::from_str("Invalid: found invalid characters in hex code")
+            }
+            ErrorCode::InvalidHexLength => {
+                JsValue::from_str("Invalid: hex code has invalid length (must be 3 or 6)")
+            }
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn hex_to_rgb(hex: &str) -> Result<RgbColour, JsValue> {
+    match hex.len() {
+        3 => {
+            let chars: Vec<char> = hex.chars().collect();
+            hex_to_rgb(&format!(
+                "{r}{r}{g}{g}{b}{b}",
+                r = chars[0],
+                g = chars[1],
+                b = chars[2]
+            ))
+        }
+        6 => {
+            let chars: Vec<char> = hex.chars().collect();
+            let parsed = (
+                hex_str_to_int(chars[0], chars[1]),
+                hex_str_to_int(chars[2], chars[3]),
+                hex_str_to_int(chars[4], chars[5]),
+            );
+            match parsed {
+                (Ok(red), Ok(green), Ok(blue)) => Ok(RgbColour { red, green, blue }),
+                _ => Err(JsValue::from(ErrorCode::InvalidHexCharacter)),
+            }
+        }
+        _ => Err(JsValue::from(ErrorCode::InvalidHexLength)),
+    }
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, colour-fun!");
+}
+
+fn hex_str_to_int(a: char, b: char) -> Result<u8, std::num::ParseIntError> {
+    u8::from_str_radix(&format!("{}{}", a, b), 16)
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,9 +3,47 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use colour_fun::*;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
 
-wasm_bindgen_test_configure!(run_in_browser);
+// wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn six_char_hex_to_rgb_white() {
+    assert_eq!(hex_to_rgb("FFFFFF"), Ok(RgbColour::new(255, 255, 255)));
+}
+
+#[wasm_bindgen_test]
+fn six_char_hex_to_rgb_pink() {
+    assert_eq!(hex_to_rgb("F43C8E"), Ok(RgbColour::new(244, 60, 142)));
+}
+
+#[wasm_bindgen_test]
+fn three_char_hex_to_rgb_black() {
+    assert_eq!(hex_to_rgb("000"), Ok(RgbColour::new(0, 0, 0)));
+}
+
+#[wasm_bindgen_test]
+fn three_char_hex_to_rgb_pink() {
+    assert_eq!(hex_to_rgb("d15"), Ok(RgbColour::new(221, 17, 85)));
+}
+
+#[wasm_bindgen_test]
+fn invalid_char_hex_to_rgb() {
+    assert_eq!(
+        hex_to_rgb("F43C8X"),
+        Err(JsValue::from(ErrorCode::InvalidHexCharacter))
+    );
+}
+
+#[wasm_bindgen_test]
+fn invalid_length_hex_to_rgb() {
+    assert_eq!(
+        hex_to_rgb("F43C"),
+        Err(JsValue::from(ErrorCode::InvalidHexLength))
+    );
+}
 
 #[wasm_bindgen_test]
 fn pass() {
